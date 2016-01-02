97b90fa5ce40a3776047c2c3c2aa39f774e2658f
Karl Hiner <karl.hiner@gmail.com>
2016-01-02 14:35:12 -0800

    Add halve/double sample-rate buttons.  Add reverse toggle (disabled for now due to sfplay bugz that I cant't find a workaround for)

--- code/main.js
+++ code/main.js
@@ -1,11 +1,12 @@
-	inlets = 4;
+inlets = 5;
 outlets = 5;
 
 /********INPUTS*****************
  * 0: note (int) OR bang (play) OR message (openSampleMap)
- * 1: sample-choice-type (int) OR sample-number (float)
- * 2: current_sample (list) OR sample-level (float)
+ * 1: sample-choice-type (int) OR sample-level (float)
+ * 2: current_sample (list)
  * 3: pick (float) OR steal_samples (int: 0 or 1)
+ * 4: reverse (int: 0 or 1)
 ********************************/
 
 /********OUTPUTS*****************
@@ -28,15 +29,17 @@ var RANDOM = 3;
 var quantized = false;
 var sample_infos_for_note = [];
 var sample_count_for_note = [];
-var sample_index_for_cue = [];
+var sample_offset_for_cue = [];
 var enabled_samples_for_note;
-var curr_sample_index_for_note;
+var curr_sample_offset_for_note;
 var active_samples; // binary list of sample indices to display as up next
 var curr_note = 0;
 var loop_type = 0;
 var nearest_note_with_samples = [];
 var steal_samples = true;
 var level_for_sample = [];
+var playback_speed_for_sample = [];
+var prev_playback_speed = 0;
 
 function createAndFillArray(length, fillValue) {
   var array = [];
@@ -46,7 +49,7 @@ function createAndFillArray(length, fillValue) {
 }
 
 function samplesToMillis(samples, sample_rate) {
-  return (1000 / sample_rate) * samples;
+  return (1000.0 / sample_rate) * samples;
 }
 
 // the played note may have no samples.
@@ -75,13 +78,22 @@ function setSampleCountForNote(sample_count, note) {
   sample_count_for_note[note] = sample_count;
 }
 
-function getSampleIndex() {
-  return curr_sample_index_for_note[curr_note];
+function getSampleOffset() {
+  return curr_sample_offset_for_note[curr_note];
 }
 
-function setSampleIndex(sample_index) {
-  outlet(4, level_for_sample[MAX_SAMPLES_PER_NOTE * curr_note + sample_index]);
-  curr_sample_index_for_note[curr_note] = sample_index;
+function getSampleIndexForNote(note) {
+  return MAX_SAMPLES_PER_NOTE * note + getSampleOffset();
+}
+
+function getSampleIndex() {
+  return getSampleIndexForNote(curr_note);
+}
+
+function setSampleOffset(sample_offset) {
+  curr_sample_offset_for_note[curr_note] = sample_offset;
+  var sample_index = getSampleIndex();
+  outlet(4, level_for_sample[sample_index]);
   updateSampleUi();
 }
 
@@ -93,68 +105,55 @@ function setEnabledSampleMask(enabled_sample_mask) {
   enabled_samples_for_note[curr_note] = enabled_sample_mask;
 }
 
-function enableSampleForNote(sample_index, note) {
-  enabled_samples_for_note[note] = enabled_samples_for_note[note] | (1 << sample_index);
+function enableSampleForNote(sample_offset, note) {
+  enabled_samples_for_note[note] = enabled_samples_for_note[note] | (1 << sample_offset);
 }
 
-function enableSample(sample_index) {
-  enableSampleForNote(sample_index, curr_note);
+function enableSample(sample_offset) {
+  enableSampleForNote(sample_offset, curr_note);
 }
 
-function isSampleEnabled(sample_index) {
-  return (getEnabledSampleMask() & (1 << sample_index)) != 0;
+function isSampleEnabled(sample_offset) {
+  return (getEnabledSampleMask() & (1 << sample_offset)) != 0;
 }
 
 function isCurrentSampleEnabled() {
-  return isSampleEnabled(getSampleIndex());
+  return isSampleEnabled(getSampleOffset());
 }
 
 function outputActiveSamples() {
-  for (var sample_index = 0; sample_index < active_samples.length; sample_index++)
-	active_samples[sample_index] = 0;
+  for (var sample_offset = 0; sample_offset < active_samples.length; sample_offset++)
+	active_samples[sample_offset] = 0;
 
-  var sample_index = getSampleIndex();
-  if (sample_index >= 0 && sample_index < active_samples.length)
-	active_samples[sample_index] = 1;
+  var sample_offset = getSampleOffset();
+  if (sample_offset >= 0 && sample_offset < active_samples.length)
+	active_samples[sample_offset] = 1;
 
   outlet(2, active_samples);
 }
 
-function findNextSampleIndex() {
+function findNextSample() {
   if (getEnabledSampleMask() == 0)
 	return; // no samples enabled
 
-  var prev_sample_index = getSampleIndex();
+  var prev_sample_offset = getSampleOffset();
   var sample_count = getSampleCount();
 
   if (loop_type == RANDOM)
-    setSampleIndex(Math.floor(Math.random() * sample_count));
-  else if (loop_type == CYCLE_X || (loop_type == CYCLE_Y && sample_count <= MAX_SAMPLES_PER_NOTE / 4))
-    setSampleIndex((prev_sample_index + 1) % Math.min(sample_count, MAX_SAMPLES_PER_NOTE));
+    setSampleOffset(Math.floor(Math.random() * sample_count));
+  else if (loop_type == CYCLE_X || (loop_type == CYCLE_Y && sample_count <= SAMPLE_ROW_SIZE))
+    setSampleOffset((prev_sample_offset + 1) % Math.min(sample_count, MAX_SAMPLES_PER_NOTE));
   else if (loop_type == CYCLE_Y) {
-	var next_sample_index = prev_sample_index + MAX_SAMPLES_PER_NOTE / 4;
-	if (next_sample_index >= Math.min(sample_count, MAX_SAMPLES_PER_NOTE))
-	  next_sample_index = ((next_sample_index % SAMPLE_ROW_SIZE) + 1) % Math.min(sample_count, SAMPLE_ROW_SIZE);
-    setSampleIndex(next_sample_index);
+	var next_sample_offset = prev_sample_offset + SAMPLE_ROW_SIZE;
+	if (next_sample_offset >= Math.min(sample_count, MAX_SAMPLES_PER_NOTE))
+	  next_sample_offset = ((next_sample_offset % SAMPLE_ROW_SIZE) + 1) % Math.min(sample_count, SAMPLE_ROW_SIZE);
+    setSampleOffset(next_sample_offset);
   }
 
   while (!isCurrentSampleEnabled())
-	setSampleIndex((getSampleIndex() + 1) % sample_count);
+	setSampleOffset((getSampleOffset() + 1) % sample_count);
 
   outputActiveSamples();
-}
-
-function updateSampleUiAndToggles() {
-  var sample_count = getSampleCount();
-
-  for (var sample_index = 0; sample_index < MAX_SAMPLES_PER_NOTE; sample_index++) {
-	var show_or_hide = (sample_index < sample_count) ? 'show' : 'hide';
-	outlet(3, 'script', show_or_hide, 'sampLed' + (sample_index + 1));
-	outlet(3, 'script', show_or_hide, 'sampToggle' + (sample_index + 1));
-	if (sample_index < sample_count)
-	  outlet(3, 'script', 'send', 'sampToggle' + (sample_index + 1), 'set', isSampleEnabled(sample_index) ? 1 : 0);
-  }
-  updateSampleUi();
 }
 
 function updateSampleUi() {
@@ -162,19 +161,66 @@ function updateSampleUi() {
 	outlet(3, 'script', 'hide', 'sampleLevel');
 	outlet(3, 'script', 'hide', 'browseSample');
 	outlet(3, 'script', 'hide', 'browseSampleLabel');
+    //outlet(3, 'script', 'hide', 'reverseToggle');
+    //outlet(3, 'script', 'hide', 'reverseLabel');
+    outlet(3, 'script', 'hide', 'halveButton');
+    outlet(3, 'script', 'hide', 'halveLabel');
+    outlet(3, 'script', 'hide', 'doubleButton');
+    outlet(3, 'script', 'hide', 'doubleLabel');
   } else {
-    outlet(3, 'script', 'send', 'sampleLevel', level_for_sample[MAX_SAMPLES_PER_NOTE * curr_note + getSampleIndex()]);
+    outlet(3, 'script', 'send', 'sampleLevel', level_for_sample[getSampleIndex()]);
     outlet(3, 'script', 'show', 'sampleLevel');
     outlet(3, 'script', 'show', 'browseSample');
     outlet(3, 'script', 'show', 'browseSampleLabel');
+    //outlet(3, 'script', 'send', 'reverseToggle', 'set', playback_speed_for_sample[getSampleIndex()] < 0 ? 1 : 0);
+    //outlet(3, 'script', 'show', 'reverseToggle');
+    //outlet(3, 'script', 'show', 'reverseLabel');
+    outlet(3, 'script', 'show', 'halveButton');
+    outlet(3, 'script', 'show', 'halveLabel');
+    outlet(3, 'script', 'show', 'doubleButton');
+    outlet(3, 'script', 'show', 'doubleLabel');
+  }
+}
+
+function updateSampleUiAndToggles() {
+  var sample_count = getSampleCount();
+
+  for (var sample_offset = 0; sample_offset < MAX_SAMPLES_PER_NOTE; sample_offset++) {
+	var show_or_hide = (sample_offset < sample_count) ? 'show' : 'hide';
+	outlet(3, 'script', show_or_hide, 'sampLed' + (sample_offset + 1));
+	outlet(3, 'script', show_or_hide, 'sampToggle' + (sample_offset + 1));
+	if (sample_offset < sample_count)
+	  outlet(3, 'script', 'send', 'sampToggle' + (sample_offset + 1), 'set', isSampleEnabled(sample_offset) ? 1 : 0);
+  }
+  updateSampleUi();
+}
+
+function incrementSample(increment) {
+  var sample_infos = sample_infos_for_note[nearestNoteWithSamples()];
+  if (sample_infos) {
+	var cue_number = getSampleIndexForNote(nearestNoteWithSamples());
+	sample_offset_for_cue[cue_number] = mod(sample_offset_for_cue[cue_number] + increment, getSampleCount());
+	var sample_info = sample_infos[sample_offset_for_cue[cue_number]];
+	if (sample_info) {
+      outlet(0, 'preload', cue_number, sample_info.sample_path, sample_info.start_time_ms, sample_info.start_time_ms + sample_info.duration_ms, 1);
+    }
   }
 }
 
 function playNextSample() {
-  findNextSampleIndex();
+  findNextSample();
   if (isCurrentSampleEnabled()) {
-	var corrected_curr_note = nearestNoteWithSamples();
-    outlet(0, MAX_SAMPLES_PER_NOTE * nearestNoteWithSamples() + getSampleIndex());
+	var playback_speed = playback_speed_for_sample[getSampleIndex()];
+	if ((prev_playback_speed <= 0 && playback_speed > 0) ||
+	    (prev_playback_speed > 0 && playback_speed < 0)) {
+	  // hack
+	  //incrementSample(0);
+	  //var now = new Date().getTime();
+      //while(new Date().getTime() < now + 100) { /* do nothing */ } 
+    }
+	outlet(1, playback_speed);
+    outlet(0, getSampleIndexForNote(nearestNoteWithSamples()));
+   	prev_playback_speed = playback_speed;
   }
 }
 
@@ -186,20 +232,8 @@ function mod(n, m) {
 
 function loadbang() {
   active_samples = createAndFillArray(MAX_SAMPLES_PER_NOTE, 0);
-  curr_sample_index_for_note = createAndFillArray(MAX_MIDI_NOTE_VALUE, 0);
+  curr_sample_offset_for_note = createAndFillArray(MAX_MIDI_NOTE_VALUE, 0);
   enabled_samples_for_note = createAndFillArray(MAX_MIDI_NOTE_VALUE, 0);
-}
-
-function incrementSample(increment) {
-  var sample_infos = sample_infos_for_note[nearestNoteWithSamples()];
-  if (sample_infos) {
-	var cue_number = MAX_SAMPLES_PER_NOTE * nearestNoteWithSamples() + getSampleIndex();
-	sample_index_for_cue[cue_number] = mod(sample_index_for_cue[cue_number] + increment, getSampleCount());
-	var sample_info = sample_infos[sample_index_for_cue[cue_number]];
-	if (sample_info) {
-      outlet(0, 'preload', cue_number, sample_info.sample_path, sample_info.start_time_ms, sample_info.start_time_ms + sample_info.duration_ms);
-    }
-  }
 }
 
 function left() {
@@ -213,18 +247,19 @@ function right() {
 function bang() {
   if (inlet == 0) {
     playNextSample();
+  } else if (inlet == 2 || inlet == 3) {
+	// divide playback rate
+	var sample_index = getSampleIndex();
+	playback_speed_for_sample[sample_index] *= (inlet == 2 ? 0.5 : 2);
+	outlet(1, playback_speed_for_sample[sample_index]);
   }
 }
 
 function msg_int(arg) {
-  if (inlet == 0) {
-	if (arg == 0)
-	  return;
+  if (inlet == 0 && arg != 0) {
 	var prev_note = curr_note;
 	curr_note = arg;
 	if (prev_note != curr_note) {
-	  var sample_rate = nearestNoteWithSamples() != curr_note ? 1 + (curr_note - nearestNoteWithSamples()) / 12.0 : 1;
-	  outlet(1, sample_rate);
 	  updateSampleUiAndToggles();
 	}
 	if (!quantized)
@@ -234,21 +269,24 @@ function msg_int(arg) {
   } else if (inlet == 2) {
 	quantized = arg > 1; // 0 == no retrigger, 1 == EOS, 2-N == quantized
   } else if (inlet == 3) {
-	steal_samples = arg == 1 ? true : false;
+	steal_samples = (arg == 1);
 	if (sample_count_for_note[curr_note] == 0) {
       updateSampleUiAndToggles();
 	}
+  } else if (inlet == 4) {
+	var sample_index = getSampleIndex();
+	playback_speed_for_sample[sample_index] = -playback_speed_for_sample[sample_index];
   }
 }
 
 function msg_float(arg) {
   if (inlet == 1) { // sample-level
-	level_for_sample[MAX_SAMPLES_PER_NOTE * curr_note + getSampleIndex()] = arg;
+	level_for_sample[getSampleIndex()] = arg;
 	outlet(4, arg);
   } else if (inlet == 3) { // pick
     if (loop_type == PICK || loop_type == REPEAT) {
-	  setSampleIndex(Math.floor(arg * (Math.min(MAX_SAMPLES_PER_NOTE, getSampleCount()) - 1)));
-	  findNextSampleIndex();
+	  setSampleOffset(Math.floor(arg * (Math.min(MAX_SAMPLES_PER_NOTE, getSampleCount()) - 1)));
+	  findNextSample();
     }
   }
 }
@@ -256,9 +294,9 @@ function msg_float(arg) {
 function list() {
   var enabled_sample_array = arrayfromargs(messagename, arguments);
   setEnabledSampleMask(0);
-  for (var sample_index = 0; sample_index < getSampleCount(); sample_index++)
-	if (enabled_sample_array[sample_index] == 1)
-	  enableSample(sample_index);
+  for (var sample_offset = 0; sample_offset < getSampleCount(); sample_offset++)
+	if (enabled_sample_array[sample_offset] == 1)
+	  enableSample(sample_offset);
 }
 
 function openSampleMap(sample_info_map_path) {
@@ -272,12 +310,12 @@ function openSampleMap(sample_info_map_path) {
 	  break;
 	var sample_info = line.split('::');
 	var sample_path = sample_info[0];
-	outlet(0, "open", sample_path);
+	outlet(0, 'open', sample_path);
 	sample_infos.push(sample_info);
   }
 
   while (f.position != f.eof) {
-	var line = f.readline(10000);
+	var line = f.readline(100000);
 	var note_and_segments = line.split(':');
 	var note = parseInt(note_and_segments[0]);
 	var segments = note_and_segments[1].split('|')
@@ -287,8 +325,8 @@ function openSampleMap(sample_info_map_path) {
     for (var i = 0; i < segments.length; i++) {
 	  var segment = segments[i];
 	  var segment_sections = segment.split(',');
-	  var sample_index = parseInt(segment_sections[0]);
-	  var sample_info = sample_infos[sample_index];
+	  var sample_offset = parseInt(segment_sections[0]);
+	  var sample_info = sample_infos[sample_offset];
 	  var sample_rate = sample_info[1];
 
 	  var sample_info_obj = {};
@@ -300,16 +338,19 @@ function openSampleMap(sample_info_map_path) {
       if (i < MAX_SAMPLES_PER_NOTE) {
   	    var cue_number = note * MAX_SAMPLES_PER_NOTE + i;
 	    level_for_sample[cue_number] = 0;
-	    sample_index_for_cue[cue_number] = i;
-	    outlet(0, 'preload', cue_number, sample_info_obj.sample_path, sample_info_obj.start_time_ms, sample_info_obj.start_time_ms + sample_info_obj.duration_ms);
+	    playback_speed_for_sample[cue_number] = 1;
+	    sample_offset_for_cue[cue_number] = i;
+	    outlet(0, 'preload', cue_number, sample_info_obj.sample_path, sample_info_obj.start_time_ms, sample_info_obj.start_time_ms + sample_info_obj.duration_ms, 1);
 	  }
 	}
   }
 
-  for (var note = 0; note <= MAX_MIDI_NOTE_VALUE; note++)
-	if (getSampleCountForNote(note) == null)
-	    setSampleCountForNote(0, note);
-	
+  for (var note = 0; note <= MAX_MIDI_NOTE_VALUE; note++) {
+	if (getSampleCountForNote(note) == null) {
+	  setSampleCountForNote(0, note);
+	}
+  }
+
   for (var note = 0; note <= MAX_MIDI_NOTE_VALUE; note++) {
 	if (getSampleCountForNote(note) == 0) {
 	  // find nearest note with samples
@@ -328,8 +369,9 @@ function openSampleMap(sample_info_map_path) {
 	}
 	var nearest_note = nearestNoteWithSamplesForNote(note);
 	var sample_count = getSampleCountForNote(nearest_note);
-	for (var sample_index = 0; sample_index < sample_count; sample_index++) {
-      enableSampleForNote(sample_index, note);
+	for (var sample_offset = 0; sample_offset < sample_count; sample_offset++) {
+      enableSampleForNote(sample_offset, note);
+	  playback_speed_for_sample[MAX_SAMPLES_PER_NOTE * note + sample_offset] = 1 + (note - nearest_note) / 12.0;
     }
   }
 }
